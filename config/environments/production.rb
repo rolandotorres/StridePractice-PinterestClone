e7276aac0209cf5b7a7a0223a6ffdb9b150560ac
rolandotorres <vorlik2004@yahoo.com>
2016-03-02 05:27:16 +0000

    revert environ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host:'rolandos-pinterestclone.herokuapp.com' }
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
   # config/environments/production.rb
   config.paperclip_defaults = {
